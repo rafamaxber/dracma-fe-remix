01f9008d04db2e6bfabd2c1881495b1ae6761921
Rafael Bernardo <rafamaxber@gmail.com>
2024-01-16 23:05:24 -0300

    feat: start with remix-pwa 🔥x

--- app/root.tsx
+++ app/root.tsx
@@ -2,12 +2,13 @@ import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
-  LiveReload,
+  // LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { useSWEffect , LiveReload } from '@remix-pwa/sw'
 
 import styles from "~/components/styles/tailwind.css";
 
@@ -17,11 +18,26 @@ export const links: LinksFunction = () => [
 ]
 
 export default function App() {
+  useSWEffect();
+
   return (
     <html lang="pt-br">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#808080" />
+        <meta name="apple-mobile-web-app-title" content="Dracma" />
+        <meta name="application-name" content="Dracma" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="msapplication-TileImage" content="/icons/mstile-144x144.png" />
+        <meta name="theme-color" content="#000000" />
+
         <Meta />
         <Links />
       </head>
